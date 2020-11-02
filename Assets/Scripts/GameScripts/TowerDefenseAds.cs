f79dfdc80a90015cc239b3d82f032b878e47818b
Kaieida <63949971+Kaieida@users.noreply.github.com>
2020-11-02 10:06:16 +0200

    Update

--- Assets/Scripts/GameScripts/TowerDefenseAds.cs
+++ Assets/Scripts/GameScripts/TowerDefenseAds.cs
@@ -17,7 +17,7 @@ public class TowerDefenseAds : MonoBehaviour
     private void RequestBanner()
     {
 #if UNITY_ANDROID
-        string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+        string adUnitId = "ca-app-pub-5365956759575502/4110255964";
 #elif UNITY_IPHONE
             string adUnitId = "ca-app-pub-3940256099942544/2934735716";
 #else
